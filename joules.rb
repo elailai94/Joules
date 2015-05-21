db89fe31ec0b55e0a7203887551d4a67f8c90c2e
Elisha Lai <elisha.lai@outlook.com>
2015-05-21 19:30:13 -0400

    Create joules.rb
    Added joules.rb to current directory.

---
+++ joules.rb
@@ -0,0 +1,18 @@
+#==============================================================================
+# Joules
+#
+# @description: Module for providing Physics formulas
+# @author: Elisha Lai
+# @version: 0.0.1 16/05/2015
+#==============================================================================
+
+# Joules module (joules.rb)
+
+require 'joules/constants'
+require 'joules/kinematics'
+require 'joules/mass_weight'
+require 'joules/density'
+require 'joules/forces'
+require 'joules/momentum_impulse'
+require 'joules/energy_work_power'
+require 'joules/pressure'
