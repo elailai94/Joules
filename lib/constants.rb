f8d5fdd2604285cfd0eab3f5dae49cf5da4db477
Elisha Lai <elisha.lai@outlook.com>
2015-05-21 19:22:28 -0400

    Create constants.rb
    Added constants.rb to current directory.

---
+++ lib/constants.rb
@@ -0,0 +1,17 @@
+#==============================================================================
+# Joules
+#
+# @description: Module for providing constants
+# @author: Elisha Lai
+# @version: 0.0.1 16/05/2015
+#==============================================================================
+
+# Constants module (constants.rb)
+
+# Speed of light in free space
+# Note: This quantity is in meters per second.
+SPEED_OF_LIGHT = 3.00e8
+
+# Acceleration of free fall
+# Note: This quantity is in meters per second squared.
+FREE_FALL_ACCELERATION = 9.81
