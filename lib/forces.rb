1399d465c5824f394f0784d07445333a9096555d
Elisha Lai <elisha.lai@outlook.com>
2015-05-21 01:36:08 -0400

    Update forces.rb

--- lib/forces.rb
+++ lib/forces.rb
@@ -14,9 +14,9 @@
 #          mass is in kilograms.
 #          acceleration is in meters per second squared.
 #     POST: Returns a Float.
-#           returned value is in kilogram meters per second squared/newtons.
+#           returned value is in newtons.
 # Purpose: Consumes two numbers, mass and acceleration. Returns the
-#   calculated force.
+# calculated force.
 def force_v1(mass, acceleration)
   return mass * acceleration.to_f
 end
@@ -24,15 +24,14 @@ end
 # force_v2: (union Int Float) (union Int Float) -> Float
 # Conditions:
 #     PRE: spring_constant >= 0
-#          spring constant is in kilograms per second squared/
-#          newtons per meter.
+#          spring constant is in newtons per meter.
 #          extension >= 0
 #          extension is in meters. 
 #     POST: Returns a Float.
 #           returned value >= 0
-#           returned value is in kilogram meters per second squared/newtons.
+#           returned value is in newtons.
 # Purpose: Consumes two numbers, spring_constant and extension. Returns the
-#   calculated force.
+# calculated force.
 def force_v2(spring_constant, extension)
   return spring_constant * extension.to_f
 end
@@ -45,23 +44,22 @@ end
 #          time > 0
 #          time is in seconds.
 #     POST: Returns a Float.
-#           returned value is in kilogram meters per second squared/newtons.
+#           returned value is in newtons.
 # Purpose: Consumes four numbers, initial_velocity, final_velocity, mass,
-#   and time. Returns the calculated force.
+# and time. Returns the calculated force.
 def force_v3(initial_velocity, final_velocity, mass, time)
   return ((final_velocity - initial_velocity) * mass) / time.to_f
 end
 
 # moment: (union Int Float) (union Int Float) -> Float
 # Conditions:
-#     PRE: force is in kilogram meters per second squared/newtons.
+#     PRE: force is in newtons.
 #          distance >= 0
 #          distance is in meters.
 #     POST: Returns a Float.
-#           returned value is in kilogram meters squared per second
-#           squared/newton metres.
+#           returned value is in newton metres.
 # Purpose: Consumes two numbers, force and distance. Returns the
-#   calculated moment.
+# calculated moment.
 def moment(force, distance)
   return force * distance.to_f
 end
