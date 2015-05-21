93d4c3301eeadac71db1a01287a43a2c47823736
Elisha Lai <elisha.lai@outlook.com>
2015-05-20 22:42:56 -0400

    Create density.rb
    Added density.rb to current directory.

---
+++ lib/density.rb
@@ -0,0 +1,24 @@
+#==============================================================================
+# Joules
+#
+# @description: Module for providing density equations
+# @author: Elisha Lai
+# @version: 0.0.1 16/05/2015
+#==============================================================================
+
+# Density module (density.rb)
+
+# density: (union Int Float) (union Int Float) -> Float
+# Conditions:
+#     PRE: mass >= 0
+#          mass is in kilograms.
+#          volume > 0
+#          volume is in meters cubed.
+#     POST: Returns a Float.
+#           returned value >= 0 
+#           returned value is in kilograms per meter cubed.
+# Purpose: Consumes two numbers, mass and volume. Returns the
+#   calculated density.
+def density(mass, volume)
+  return mass / volume.to_f
+end
