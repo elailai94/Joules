b4cd81da5bfd00bfac65984c8dbb479c3311fa00
Elisha Lai <elisha.lai@outlook.com>
2015-05-21 19:23:59 -0400

    Update mass_weight.rb

--- lib/mass_weight.rb
+++ lib/mass_weight.rb
@@ -1,7 +1,7 @@
 #==============================================================================
 # Joules
 #
-# @description: Module for providing mass and weight equations
+# @description: Module for providing mass and weight formulas
 # @author: Elisha Lai
 # @version: 0.0.1 16/05/2015
 #==============================================================================
