15e72faae5f1b3c6f52eea1e175373947f642e9e
Elisha Lai <elisha.lai@outlook.com>
2015-05-22 16:56:04 -0400

    Update constants.rb
    Added more Physics constants to current module.

--- lib/joules/constants.rb
+++ lib/joules/constants.rb
@@ -3,7 +3,7 @@
 #
 # @description: Module for providing constants
 # @author: Elisha Lai
-# @version: 0.0.2 16/05/2015
+# @version: 0.0.3 16/05/2015
 #==============================================================================
 
 # Constants module (constants.rb)
@@ -11,6 +11,50 @@
 # Speed of light in free space
 # Note: This quantity is in meters per second.
 SPEED_OF_LIGHT = 3.00e8
+
+# Permeability of free space
+# Note: This quantity is in henries per meter.
+FREE_SPACE_PERMEABILITY = 4 * Math::PI * 1e-7
+
+# Permittivity of free space
+# Note: This quantity is in farads per meter.
+FREE_SPACE_PERMITTIVITY = 8.85e-12
+
+# Elementary charge
+# Note: This quantity is in coulombs.
+ELEMENTARY_CHARGE = 1.6e-19
+
+# Planck constant
+# Note: This quantity is in joule seconds.
+PLANCK_CONSTANT = 6.63e-34
+
+# Unified atomic mass unit
+# Note: This quantiy is in kilograms.
+UNIFIED_ATOMIC_MASS_UNIT = 1.66e-27
+
+# Rest mass of electron
+# Note: This quantity is in kilograms.
+ELECTRON_MASS = 9.11e-31
+
+# Rest mass of proton
+# Note: This quantity is in kilograms.
+PROTON_MASS = 1.67e-27
+
+# Molar gas constant
+# Note: This quantity is in joules per kelvin mole.
+MOLAR_GAS_CONSTANT = 8.31
+
+# Avogadro constant
+# Note: This quantity is in per mole. 
+AVOGADRO_CONSTANT = 6.02e23
+
+# Boltzmann constant
+# Note: This quantity is in joules per kelvin.
+BOLTZMANN_CONSTANT = 1.38e-23
+
+# Gravitational constant
+# Note: This quantity is in newton metres squared per kilogram squared.
+GRAVITATIONAL_CONSTANT = 6.67e-11
 
 # Acceleration of free fall
 # Note: This quantity is in meters per second squared.
