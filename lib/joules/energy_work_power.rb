bc733e64426a8948426c71942f06dd4e0f3cd429
Elisha Lai <elisha.lai@outlook.com>
2015-05-22 12:24:22 -0400

    Update energy_work_power.rb

--- lib/joules/energy_work_power.rb
+++ lib/joules/energy_work_power.rb
@@ -3,7 +3,7 @@
 #
 # @description: Module for providing energy, work, and power formulas
 # @author: Elisha Lai
-# @version: 0.0.1 16/05/2015
+# @version: 0.0.2 16/05/2015
 #==============================================================================
 
 # Energy, work, and power module (energy_work_power.rb)
