680d6e07979a11b342a174d50dbeabb00c1628f2
Elisha Lai <elisha.lai@outlook.com>
2015-05-23 17:26:24 -0400

    Update density.rb

--- lib/joules/density.rb
+++ lib/joules/density.rb
@@ -3,22 +3,20 @@
 #
 # @description: Module for providing density formulas
 # @author: Elisha Lai
-# @version: 0.0.3 16/05/2015
+# @version: 0.1.0 23/05/2015
 #==============================================================================
 
 # Density module (density.rb)
 
-# density: (union Int Float) (union Int Float) -> Float
-# Conditions:
-#     PRE: mass >= 0
-#          mass is in kilograms.
-#          volume > 0
-#          volume is in meters cubed.
-#     POST: Returns a Float.
-#           returned value >= 0 
-#           returned value is in kilograms per meter cubed.
-# Purpose: Consumes two numbers, mass and volume. Returns the
-# calculated density.
+# Calculates the density given mass and volume.
+# @param mass [Int, Float]
+#   mass >= 0; mass is in kilograms
+# @param volume [Int, Float]
+#   volume > 0; volume is in meters cubed
+# @return [Float]
+#   returned value >= 0; returned value is in kilograms per meter cubed
+# @example
+#   density(8.96, 0.002) #=> 4480.0   
 def density(mass, volume)
   return mass / volume.to_f
 end
