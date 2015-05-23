4430028feccec39087f641bbe2f5ccf0435bfd56
Elisha Lai <elisha.lai@outlook.com>
2015-05-23 16:46:01 -0400

    Update constants.rb
    Added Stefan constant, Wien constant, and rest mass of neutron to current module.

--- lib/joules/constants.rb
+++ lib/joules/constants.rb
@@ -3,59 +3,71 @@
 #
 # @description: Module for providing constants
 # @author: Elisha Lai
-# @version: 0.0.3 16/05/2015
+# @version: 0.1.0 23/05/2015
 #==============================================================================
 
 # Constants module (constants.rb)
 
-# Speed of light in free space
-# Note: This quantity is in meters per second.
+# Speed of light in free space.
+# @note This quantity is in meters per second.
 SPEED_OF_LIGHT = 3.00e8
 
-# Permeability of free space
-# Note: This quantity is in henries per meter.
+# Permeability of free space.
+# @note This quantity is in henries per meter.
 FREE_SPACE_PERMEABILITY = 4 * Math::PI * 1e-7
 
-# Permittivity of free space
-# Note: This quantity is in farads per meter.
+# Permittivity of free space.
+# @note This quantity is in farads per meter.
 FREE_SPACE_PERMITTIVITY = 8.85e-12
 
-# Elementary charge
-# Note: This quantity is in coulombs.
+# Elementary charge.
+# @note This quantity is in coulombs.
 ELEMENTARY_CHARGE = 1.6e-19
 
-# Planck constant
-# Note: This quantity is in joule seconds.
+# Planck constant.
+# @note This quantity is in joule seconds.
 PLANCK_CONSTANT = 6.63e-34
 
-# Unified atomic mass unit
-# Note: This quantiy is in kilograms.
+# Unified atomic mass unit.
+# @note This quantiy is in kilograms.
 UNIFIED_ATOMIC_MASS_UNIT = 1.66e-27
 
-# Rest mass of electron
-# Note: This quantity is in kilograms.
+# Rest mass of electron.
+# @note This quantity is in kilograms.
 ELECTRON_MASS = 9.11e-31
 
-# Rest mass of proton
-# Note: This quantity is in kilograms.
+# Rest mass of proton.
+# @note This quantity is in kilograms.
 PROTON_MASS = 1.67e-27
 
-# Molar gas constant
-# Note: This quantity is in joules per kelvin mole.
+# Rest mass of neutron.
+# @note This quantity is in kilograms.
+NEUTRON_MASS = 1.67e-27
+
+# Molar gas constant.
+# @note This quantity is in joules per kelvin mole.
 MOLAR_GAS_CONSTANT = 8.31
 
-# Avogadro constant
-# Note: This quantity is in per mole. 
+# Avogadro constant.
+# @note This quantity is in per mole. 
 AVOGADRO_CONSTANT = 6.02e23
 
-# Boltzmann constant
-# Note: This quantity is in joules per kelvin.
+# Boltzmann constant.
+# @note This quantity is in joules per kelvin.
 BOLTZMANN_CONSTANT = 1.38e-23
 
-# Gravitational constant
-# Note: This quantity is in newton metres squared per kilogram squared.
+# Stefan constant.
+# @note This quantity is in watts per meter squared kelvin to the fourth power.
+STEFAN_CONSTANT = 5.67e-8
+
+# Wien constant.
+# @note This quantity is in meter kelvins.
+WIEN_CONSTANT = 2.9e-3
+
+# Gravitational constant.
+# @note This quantity is in newton metres squared per kilogram squared.
 GRAVITATIONAL_CONSTANT = 6.67e-11
 
-# Acceleration of free fall
-# Note: This quantity is in meters per second squared.
+# Acceleration of free fall.
+# @note This quantity is in meters per second squared.
 FREE_FALL_ACCELERATION = 9.81
