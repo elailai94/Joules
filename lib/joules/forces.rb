e98faa3145a86e97b1b81ce451ec5b01025b28e1
Elisha Lai <elisha.lai@outlook.com>
2015-05-23 18:58:36 -0400

    Update forces.rb

--- lib/joules/forces.rb
+++ lib/joules/forces.rb
@@ -3,63 +3,66 @@
 #
 # @description: Module for providing forces formulas
 # @author: Elisha Lai
-# @version: 0.0.3 16/05/2015
+# @version: 0.1.0 23/05/2015
 #==============================================================================
 
 # Forces module (forces.rb)
 
-# force_v1: (union Int Float) (union Int Float) -> Float
-# Conditions:
-#     PRE: mass >= 0
-#          mass is in kilograms.
-#          acceleration is in meters per second squared.
-#     POST: Returns a Float.
-#           returned value is in newtons.
-# Purpose: Consumes two numbers, mass and acceleration. Returns the
-# calculated force.
+# Calculates the force given mass and acceleration.
+# @param mass [Int, Float]
+#   mass >= 0; mass is in kilograms
+# @param acceleration [Int, Float]
+#   acceleration is in meters per second squared
+# @return [Float]
+#   returned value is in newtons
+# @example
+#   force_v1(120, 2.67) #=> 320.4
+# @note There are two other methods for calculating force. 
 def force_v1(mass, acceleration)
   return mass * acceleration.to_f
 end
 
-# force_v2: (union Int Float) (union Int Float) -> Float
-# Conditions:
-#     PRE: spring_constant >= 0
-#          spring constant is in newtons per meter.
-#          extension >= 0
-#          extension is in meters. 
-#     POST: Returns a Float.
-#           returned value >= 0
-#           returned value is in newtons.
-# Purpose: Consumes two numbers, spring_constant and extension. Returns the
-# calculated force.
+# Calculates the force given spring constant and extension.
+# @param spring_constant [Int, Float]
+#   spring_constant >= 0; spring_constant is in newtons per meter
+# @param extension [Int, Float]
+#   extension >= 0; extension is in meters
+# @return [Float]
+#   returned value >= 0; returned value is in newtons
+# @example
+#   force_v2(81.75, 2.4) #=> 196.2
+# @note There are two other methods for calculating force.
 def force_v2(spring_constant, extension)
   return spring_constant * extension.to_f
 end
 
-# force_v3: (union Int Float) (union Int Float) -> Float
-# Conditions:
-#     PRE: initial_velocity and final_velocity are in meters per second.
-#          mass >= 0
-#          mass is in kilograms.
-#          time > 0
-#          time is in seconds.
-#     POST: Returns a Float.
-#           returned value is in newtons.
-# Purpose: Consumes four numbers, initial_velocity, final_velocity, mass,
-# and time. Returns the calculated force.
+# Calculates the force given initial velocity, final velocity, mass, and time.
+# @param initial_velocity [Int, Float]
+#   initial_velocity is in meters per second
+# @param final_velocity [Int, Float]
+#   final_velocity is in meters per second
+# @param mass [Int, Float]
+#   mass >= 0; mass is in kilograms
+# @param time [Int, Float]
+#   time > 0; time is in seconds
+# @return [Float]
+#   returned value is in newtons
+# @example
+#   force_v3(20, 35, 50, 2.4) #=> 312.5
+# @note There are two other methods for calculating force.
 def force_v3(initial_velocity, final_velocity, mass, time)
   return ((final_velocity - initial_velocity) * mass) / time.to_f
 end
 
-# moment: (union Int Float) (union Int Float) -> Float
-# Conditions:
-#     PRE: force is in newtons.
-#          distance >= 0
-#          distance is in meters.
-#     POST: Returns a Float.
-#           returned value is in newton metres.
-# Purpose: Consumes two numbers, force and distance. Returns the
-# calculated moment.
+# Calculates the moment given force and distance.
+# @param force [Int, Float]
+#   force is in newtons
+# @param distance [Int, Float]
+#   distance >= 0; distance is in meters
+# @return [Float]
+#   returned value is in newton metres
+# @example
+#   moment(23, 4.5) #=> 103.5
 def moment(force, distance)
   return force * distance.to_f
 end
