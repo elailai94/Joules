96fadda2a0316857c4d8f4b367c9734e5d49f5e4
Elisha Lai <elisha.lai@outlook.com>
2015-05-23 21:07:05 -0400

    Update kinematics.rb

--- lib/joules/kinematics.rb
+++ lib/joules/kinematics.rb
@@ -3,122 +3,128 @@
 #
 # @description: Module for providing kinematics formulas
 # @author: Elisha Lai
-# @version: 0.0.3 16/05/2015
+# @version: 0.1.0 23/05/2015
 #==============================================================================
 
 # Kinematics module (kinematics.rb)
 
-# avg_speed: (union Int Float) (union Int Float) -> Float
-# Conditions:
-#     PRE: distance >= 0
-#          distance is in meters.
-#          time > 0
-#          time is in seconds.
-#     POST: Returns a Float.
-#           returned value >= 0
-#           returned value is in meters per second.
-# Purpose: Consumes two numbers, distance and time. Returns the
-# calculated average speed.
+# Calculates the average speed given distance and time.
+# @param distance [Int, Float]
+#   distance >= 0; distance is in meters
+# @param time [Int, Float]
+#   time > 0; time is in seconds
+# @return [Float]
+#   returned value >= 0; returned value is in meters per second
+# @example
+#   avg_speed(30, 2.4) #=> 12.5
 def avg_speed(distance, time)
   return distance / time.to_f 
 end
 
-# avg_velocity: (union Int Float) (union Int Float) -> Float
-# Conditions:
-#     PRE: displacement is in meters.
-#          time > 0
-#          time is in seconds.
-#     POST: Returns a Float.
-#           returned value is in meters per second.
-# Purpose: Consumes two numbers, displacement and time. Returns the
-# calculated average velocity.
+# Calculates the average velocity given displacement and time.
+# @param displacement [Int, Float]
+#   displacement is in meters
+# @param time [Int, Float]
+#   time > 0; time is in seconds
+# @return [Float]
+#   returned value is in meters per second
+# @example
+#   avg_velocity(180, 4.8) #=> 37.5 
 def avg_velocity(displacement, time)
   return displacement / time.to_f
 end
 
-# acceleration: (union Int Float) (union Int Float) (union Int Float)
-# -> Float
-# Conditions:
-#     PRE: initial_velocity and final_velocity are in meters per second.
-#          time > 0
-#          time is in seconds.
-#     POST: Returns a Float.
-#           returned value is in meters per second squared.
-# Purpose: Consumes three numbers: initial_velocity, final_velocity,
-# and time. Returns the calculated acceleration.
+# Calculates the acceleration given initial velocity, final velocity, and time.
+# @param initial_velocity [Int, Float]
+#   initial_velocity is in meters per second
+# @param final_velocity [Int, Float]
+#   final_velocity is in meters per second
+# @param time [Int, Float]
+#   time > 0; time is in seconds
+# @return [Float]
+#   returned value is in meters per second squared
+# @example
+#   acceleration(20, 35, 2.4) #=> 6.25
 def acceleration(initial_velocity, final_velocity, time)
   return (final_velocity - initial_velocity) / time.to_f
 end
 
-# final_velocity_v1: (union Int Float) (union Int Float) (union Int Float)
-# -> Float
-# Conditions: 
-#     PRE: initial_velocity is in meters per second.
-#          acceleration is in meters per second squared.
-#          time >= 0
-#          time is in seconds.
-#     POST: Returns a Float.
-#           returned value is in meters per second.
-# Purpose: Consumes three numbers: initial_velocity, acceleration, and
-# time. Returns the calculated final velocity.
+# Calculates the final velocity given initial velocity, acceleration, and time.
+# @param initial_velocity [Int, Float]
+#   initial_velocity is in meters per second
+# @param acceleration [Int, Float]
+#   acceleration is in meters per second squared
+# @param time [Int, Float]
+#   time >= 0; time is in seconds
+# @return [Float]
+#   returned value is in meters per second
+# @example
+#   final_velocity_v1(20, 6.25, 2.4) #=> 35.0
+# @note There is one other method for calculating final velocity.
 def final_velocity_v1(initial_velocity, acceleration, time)
   return initial_velocity + (acceleration * time.to_f)
 end
 
-# final_velocity_v2: (union Int Float) (union Int Float) (union Int Float)
-# -> Float
-# Conditions:
-#     PRE: initial_velocity is in meters per second.
-#          acceleration is in meters per second squared.
-#          displacement is in meters.
-#     POST: Returns a Float.
-#           returned value is in meters per second.
-# Purpose: Consumes three numbers: initial_velocity, acceleration, and
-# displacement. Returns the calculated final velocity.
+# Calculates the final velocity given initial velocity, acceleration, and displacement.
+# @param initial_velocity [Int, Float]
+#   initial_velocity is in meters per second
+# @param acceleration [Int, Float]
+#   acceleration is in meters per second squared
+# @param displacement [Int, Float]
+#   displacement is in meters
+# @return [Float]
+#   returned value is in meters per second
+# @example
+#   final_velocity_v2(20, 6.25, 66) #=> 35.0
+# @note There is one other method for calculating final velocity.
 def final_velocity_v2(initial_velocity, acceleration, displacement)
   return ((initial_velocity ** 2) + (2 * acceleration * displacement)) ** 0.5
 end
 
-# displacement_v1: (union Int Float) (union Int Float) (union Int Float)
-# -> Float
-# Conditions:
-#     PRE: initial_velocity and final_velocity are in meters per second.
-#          time >= 0
-#          time is in seconds.
-#     POST: Returns a Float.
-#           returned value is in meters.
-# Purpose: Consumes three numbers: initial_velocity, final_velocity, and
-# time. Returns the calculated displacement.
+# Calculates the displacement given initial velocity, final velocity, and time.
+# @param initial_velocity [Int, Float]
+#   initial_velocity is in meters per second
+# @param final_velocity [Int, Float]
+#   final_velocity is in meters per second
+# @param time [Int, Float]
+#   time >= 0; time is in seconds
+# @return [Float]
+#   returned value is in meters
+# @example
+#   displacement_v1(20, 35, 2.4) #=> 66.0
+# @note There are two other methods for calculating displacement.
 def displacement_v1(initial_velocity, final_velocity, time)
   return 0.5 * (initial_velocity + final_velocity) * time
 end
 
-# displacement_v2: (union Int Float) (union Int Float) (union Int Float)
-# -> Float
-# Conditions:
-#     PRE: initial_velocity is in meters per second.
-#          acceleration is in meters per second squared.
-#          time >= 0
-#          time is in seconds.
-#     POST: Returns a Float.
-#           returned value is in meters.
-# Purpose: Consumes three numbers: initial_velocity, acceleration, and
-# time. Returns the calculated displacement.
+# Calculates the displacement given initial velocity, acceleration, and time.
+# @param initial_velocity [Int, Float]
+#   initial_velocity is in meters per second
+# @param acceleration [Int, Float]
+#   acceleration is in meters per second squared
+# @param time [Int, Float]
+#   time >= 0; time is in seconds
+# @return [Float]
+#   returned value is in meters
+# @example
+#   displacement_v2(20, 6.25, 2.4) #=> 66.0
+# @note There are two other methods for calculating displacement.
 def displacement_v2(initial_velocity, acceleration, time)
   return (initial_velocity * time) + (0.5 * acceleration * (time ** 2))
 end
 
-# displacement_v3: (union Int Float) (union Int Float) (union Int Float)
-# -> Float
-# Conditions:
-#     PRE: final_velocity is in meters per second.
-#          acceleration is in meters per second squared.
-#          time >= 0
-#          time is in seconds.
-#     POST: Returns a Float.
-#           returned value is in meters.
-# Purpose: Consumes three numbers: final_velocity, acceleration, and
-# time. Returns the calculated displacement.
+# Calculates the displacement given final velocity, acceleration, and time.
+# @param final_velocity [Int, Float]
+#   final_velocity is in meters per second
+# @param acceleration [Int, Float]
+#   acceleration is in meters per second squared
+# @param time [Int, Float]
+#   time >= 0; time is in seconds
+# @return [Float]
+#   returned value is in meters
+# @example
+#   displacement_v3(35, 6.25, 2.4) #=> 66.0
+# @note There are two other methods for calculating displacement.
 def displacement_v3(final_velocity, acceleration, time)
   return (final_velocity * time) - (0.5 * acceleration * (time ** 2))
 end
