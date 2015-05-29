770c706633e9bcbae0132abc7529aa8da89fe5d7
Elisha Lai <elisha.lai@outlook.com>
2015-05-29 16:21:56 -0400

    Update quantum.rb

--- lib/joules/quantum.rb
+++ lib/joules/quantum.rb
@@ -20,3 +20,26 @@ require_relative 'constants'
 def photon_energy(frequency)
   return PLANCK_CONSTANT * frequency
 end
+
+# Calculates the energy given mass.
+# @param mass [Int, Float]
+#   mass >= 0; mass is in kilograms
+# @return [Float]
+#   return value >= 0; return value is in joules
+# @example
+#   energy_v4(60.5) #=> 5.445e+18
+# @note There are three other methods for calculating energy.
+def energy_v4(mass)
+  return mass * (SPEED_OF_LIGHT ** 2)
+end
+
+# Calculates the half-life of a decaying quantity given decay constant.
+# @param decay_constant [Int, Float]
+#   decay_constant != 0; decay_constant is in per second
+# @return [Float]
+#   return value is in seconds
+# @example
+#   half_life(7.7e4) #=> 9.001911435843445e-06
+def half_life(decay_constant)
+  return Math.log(2) / decay_constant
+end
