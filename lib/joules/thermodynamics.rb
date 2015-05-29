1c8a5d0e2b88ccca060ad454c29ca144468d8e74
Elisha Lai <elisha.lai@outlook.com>
2015-05-28 23:26:53 -0400

    Update thermodynamics.rb

--- lib/joules/thermodynamics.rb
+++ lib/joules/thermodynamics.rb
@@ -3,7 +3,7 @@
 #
 # @description: Module for providing thermodynamics formulas
 # @author: Elisha Lai
-# @version: 0.1.0 23/05/2015
+# @version: 0.2.0 25/05/2015
 #==============================================================================
 
 # Thermodynamics module (thermodynamics.rb)
@@ -16,10 +16,10 @@
 # @param temperature_change [Int, Float]
 #   temperature_change is in celcius
 # @return [Float]
-#   returned value is in joules
+#   return value is in joules
 # @example
 #   energy_v1(500, 2.46, 3.6) #=> 4428.0
-# @note There is one other method for calculating energy.
+# @note There are two other methods for calculating energy.
 def energy_v1(mass, specific_heat_capacity, temperature_change)
   return mass * specific_heat_capacity * temperature_change.to_f
 end
@@ -30,10 +30,10 @@ end
 # @param specific_latent_heat [Int, Float]
 #   specific_latent_heat >= 0; specific_latent_heat is in joules per kilogram
 # @return [Float]
-#   returned value is in joules
+#   return value is in joules
 # @example
 #   energy_v2(84.3, 72.1) #=> 6078.03
-# @note There is one other method for calculating energy.
+# @note There are two other methods for calculating energy.
 def energy_v2(mass, specific_latent_heat)
   return mass * specific_latent_heat.to_f
 end
