c3a92dc868b8eb970cd82e97f8f675f32e6aa0ca
Elisha Lai <elisha.lai@outlook.com>
2015-05-28 23:13:09 -0400

    Update electricity.rb

--- lib/joules/electricity.rb
+++ lib/joules/electricity.rb
@@ -51,6 +51,83 @@ def resistance_v2(resistivity, wire_length, cross_sectional_area)
   return (resistivity * wire_length) / cross_sectional_area.to_f
 end
 
+# Calculates the total resistance of resistors in series.
+# @param resistances [Array<Int, Float>]
+#   each resistance in resistances is in ohms
+# @return [Float]
+#   return value is in ohms
+# @example
+#   resistance_in_series([10, 5, 3.4, 6.3]) #=> 24.7
+def resistance_in_series(resistances)
+  total_resistance = 0
+  resistances.each do |resistance|
+    total_resistance += resistance
+  end
+  return total_resistance.to_f
+end
+
+# Calculates the total resistance of resistors in parallel.
+# @param resistances [Array<Int, Float>]
+#   each resistance in resistances != 0; each resistance in resistances is in ohms
+# @return [Float]
+#   return value is in ohms
+# @example
+#   resistance_in_parallel([0.5, 0.25, 0.125]) #=> 0.07142857142857142
+def resistance_in_parallel(resistances)
+  if resistances.empty?
+    raise ArgumentError.new('empty list of resistances')
+  end
+  total_resistance = 0
+  resistances.each do |resistance|
+    total_resistance += (1 / resistance.to_f)
+  end
+  return 1 / total_resistance.to_f
+end
+
+# Calculates the total capacitance given charge and voltage.
+# @param charge [Int, Float]
+#   charge is in coulombs
+# @param voltage [Int, Float]
+#   voltage != 0; voltage is in volts
+# @return [Float]
+#   return value is in farads
+def capacitance(charge, voltage)
+  return charge / voltage.to_f
+end
+
+# Calculates the total capacitance of capacitors in series.
+# @param capacitances [Array<Int, Float>]
+#   each capacitance in resistances != 0; each capacitance in capacitances is in farads
+# @return [Float]
+#   return value is in farads
+# @example
+#   capacitance_in_series([0.5, 0.25, 0.125]) #=> 0.07142857142857142
+def capacitance_in_series(capacitances)
+  if capacitances.empty?
+    raise ArgumentError.new('empty list of capacitances')
+  end
+  total_capacitance = 0
+  capacitances.each do |capacitance|
+  	total_capacitance += (1 / capacitance.to_f)
+  end
+  return 1 / total_capacitance.to_f
+end
+
+# Calculates the total capacitance in parallel given capacitances.
+# @param capacitances [Array<Int, Float>]
+#   each capacitance in capacitances is in farads
+# @return [Float]
+#   return value is in farads
+# @example
+#   capacitance_in_parallel([10, 5, 3.4, 6.3]) #=> 24.7
+def capacitance_in_parallel(capacitances)
+  total_capacitance = 0
+  capacitances.each do |capacitance|
+  	total_capacitance += capacitance
+  end
+  return total_capacitance.to_f
+end
+
 # Calculates the voltage given energy and charge.
 # @param energy [Int, Float]
 #   energy is in joules
@@ -111,7 +188,7 @@ end
 # @param voltage [Int, Float]
 #   voltage is in volts
 # @param resistance [Int, Float]
-#   resistance is in ohms
+#   resistance != 0; resistance is in ohms
 # @return [Float]
 #   return value is in watts
 # @example
@@ -119,4 +196,20 @@ end
 # @note There are three other methods for calculating power.
 def power_v4(voltage, resistance)
   return (voltage ** 2.0) / resistance
+end
+
+# Calculates the energy given voltage, current, and time.
+# @param voltage [Int, Float]
+#   voltage is in volts
+# @param current [Int, Float]
+#   current is in amperes
+# @param time [Int, Float]
+#   time >= 0; time is in seconds
+# @return [Float]
+#   return value is in joules
+# @example
+#   energy_v3(1.8, 0.6, 5) #=> 5.4
+# @note There are two other methods for calculating energy.
+def energy_v3(voltage, current, time)
+  return power_v2(voltage, current) * time
 end
