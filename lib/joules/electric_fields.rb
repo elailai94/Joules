7e1ffb2c93039fa1fde9d78852e9feb1b0014bc9
Elisha Lai <elisha.lai@outlook.com>
2015-06-01 22:52:15 -0400

    Minor changes to Electric Fields module.

--- lib/joules/electric_fields.rb
+++ lib/joules/electric_fields.rb
@@ -20,12 +20,12 @@ module Joules
   #   return value is in newtons per coulomb/volts per metre
   # @example
   #   electric_field_strength_v1(9, 0.1) #=> 90.0
-  # @note There is one other method for calculating electric field strength.
+  # @note There are two other method for calculating electric field strength.
   def electric_field_strength_v1(voltage, distance)
   	return voltage / distance.to_f
   end
 
-  # Calculates the electric field strength given force and charge.
+  # Calculates the electric field strength for an uniform field given force and charge.
   # @param force [Int, Float]
   #   force is in newtons
   # @param charge [Int, Float]
@@ -34,9 +34,23 @@ module Joules
   #   return value is in newtons per coulomb/volts per metre
   # @example
   #   electric_field_strength_v2(50, 1.3e-6) #=> 38461538.461538464
-  # @note There is one other method for calculating electric field strength.
+  # @note There are two other method for calculating electric field strength.
   def electric_field_strength_v2(force, charge)
     return force / charge.to_f
+  end
+
+  # Calculates the electric field strength for a radial field given charge and distance.
+  # @param charge [Int, Float]
+  #   charge is in coulombs
+  # @param distance [Int, Float]
+  #   distance > 0; distance is in meters
+  # @return [Float]
+  #   return value is in newtons per coulomb/volts per metre
+  # @example
+  #   electric_field_strength_v3(3.2e-19, 0.2) #=> 7.193443755565889e-08
+  # @note There are two other method for calculating electric field strength.
+  def electric_field_strength_v3(charge, distance)
+    return charge / (4 * Math::PI * FREE_SPACE_PERMITTIVITY * (distance ** 2))
   end
 
   # Calculates the electric potential given charge and distance.
