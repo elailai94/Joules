d996f86ae260fa1a7496141ddac116a10b081a01
Elisha Lai <elisha.lai@outlook.com>
2015-06-12 18:20:12 -0400

    Added more tests to the test module.

--- test/test_joules.rb
+++ test/test_joules.rb
@@ -14,6 +14,31 @@ include Joules
 
 EPSILON = 0.00001
 
+# Unit tests for circular motion module
+class CircularMotionTests < Test::Unit::TestCase
+  def test_angular_velocity_v1_extreme_1
+    assert_in_epsilon(0, angular_velocity_v1(0,0.001), EPSILON)
+  end
+
+  def test_angular_velocity_v1_extreme_2
+    assert_in_epsilon(1.03010, angular_velocity_v1(928476103, 901348726), EPSILON)
+  end
+
+  def test_angular_velocity_v1_big_divisor
+    assert_in_epsilon(6.32386e-09, angular_velocity_v1(5.7, 901348726), EPSILON)
+  end
+
+  def test_angular_velocity_v1_zero_division
+    assert_raise(ZeroDivisionError) do
+      angular_velocity_v1(1, 0)
+    end
+  end
+
+  def test_angular_velocity_v1_example
+  	assert_in_epsilon(3, angular_velocity_v1(9, 3), EPSILON)
+  end
+end
+
 # Unit tests for kinematics module
 class KinematicsTests < Test::Unit::TestCase
   def test_avg_speed_extreme_1
@@ -26,5 +51,11 @@ class KinematicsTests < Test::Unit::TestCase
 
   def test_avg_speed_big_divisor
     assert_in_epsilon(6.32386e-09, avg_speed(5.7, 901348726), EPSILON)
+  end
+
+  def test_avg_speed_zero_division
+    assert_raise(ZeroDivisionError) do
+      avg_speed(1, 0)
+    end
   end
 end
